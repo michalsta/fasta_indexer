419d39aa0d44eca5e6fdc1f20ff0c59a43fc7255
Michał Startek <mist@mimuw.edu.pl>
2020-08-07 12:15:08 +0200

    Code cleanup

--- src/Python/fasta_index.py
+++ src/Python/fasta_index.py
@@ -5,51 +5,75 @@ import bisect
 
 
 class FastaIndexer:
+    '''Allows fast retrieval of proteins in a given mass range.'''
+
     def __init__(self, fasta_path, idx_path = None, treat_as_sorted = None):
+        '''Construct a fast indexer for retrieval of proteins from FASTA by mass.
+
+        The FASTA file should have been previously indexed and preferably sorted by
+        the mkndex or mkindex_nosort tools. The mass of the protein is defined as
+        the average mass over possible isotopic compositions, using Earth matter
+        isotopic abundances.
+
+        Note
+        ----
+        The optional parameters idx_path and treat_as_sorted must either be both
+        provided or both omitted.
+
+        Parameters
+        ----------
+        fasta_path : str
+            Path to the (possibly sorted) FASTA file, containing the indexed proteins.
+            If the other two arguments are not provided, some simple logic is used to
+            try and find the sorted version if present
+        idx_path : str, optional
+            Path to the .fasta.idx file containing the index
+        treat_as_sorted : bool, optional
+            Whether to treat the FASTA file as sorted by mass (created by mkindex tool,
+            rather than left as-is and only indexed by mkindex_nosorted)
+
+        '''
 
         if (idx_path is None) != (treat_as_sorted is None):
             raise Exception("Either all optional arguments must be provided, or none.")
 
         if idx_path is not None and treat_as_sorted is not None:
-            self.do_setup(fasta_path, idx_path)
+            self._do_setup(fasta_path, idx_path)
             self.sorted = treat_as_sorted
             return
 
         try:
-            self.do_setup(fasta_path + ".sorted")
+            self._do_setup(fasta_path + ".sorted")
         except (OSError, IOError) as e:
-            self.do_setup(fasta_path)
+            self._do_setup(fasta_path)
 
         self.sorted = self.fasta_path.endswith(".sorted")
 
 
 
-    def do_setup(self, fasta_path, idx_path = None):
-        self.fasta_fh = open(fasta_path)
-        if idx_path is None:
-            idx_path = fasta_path + ".idx"
-        self.idx_fh = open(idx_path)
-        self.idx = mmap.mmap(self.idx_fh.fileno(), 0, access = mmap.ACCESS_READ)
-        self.fasta_path = fasta_path
-        self.idx_path = idx_path
-        self.no_entries = len(self.idx) // 16
-        self.min_mass = self.idkey_at(0)[0]
-        self.max_mass = self.idkey_at(self.no_entries-1)[0]
-        try:
-            self.idx.madvise(mmap.MADV_RANDOM)
-        except Exception:
-            pass
-        
-    def idkey_at(self, i):
-        return struct.unpack("dQ", self.idx[i*16:i*16+16])
-
     def __getitem__(self, idx):
-        return self.idkey_at(idx)[0]
+        '''Returns the file offset at which ith protein starts in FASTA'''
+        return self._idkey_at(idx)[0]
 
     def __len__(self):
+        '''Number of proteins in the FASTA file'''
         return self.no_entries
 
     def search(self, start_mass, end_mass):
+        '''Returns a generator of proteins within the given mass range.
+
+        The generator returns a sequence of pairs (Python tuples): containing
+        the header (FASTA protein name) and the sequence going over all proteins.
+
+        Paramters
+        ---------
+        start_mass : float
+            Lower end of masses of proteins that need to be returned
+        end_mass : float
+            Upper end of masses of proteins that need to be returned
+
+        '''
+
         start_mass = max(start_mass, self.min_mass)
         end_mass = min(end_mass, self.max_mass)
         left_idx = bisect.bisect_left(self, start_mass)
@@ -60,13 +84,13 @@ class FastaIndexer:
         if right_idx == left_idx:
             return
 
-        position = self.idkey_at(left_idx)[1]
+        position = self._idkey_at(left_idx)[1]
         self.fasta_fh.seek(position, os.SEEK_SET)
         assert self.fasta_fh.read(1) == '>'
 
         for i in range(left_idx, right_idx):
             if not self.sorted:
-                position = self.idkey_at(i)[1]
+                position = self._idkey_at(i)[1]
                 self.fasta_fh.seek(position, os.SEEK_SET)
                 assert self.fasta_fh.read() == '>'
             header = self.fasta_fh.readline()[:-1]
@@ -82,6 +106,26 @@ class FastaIndexer:
             if len(c) == 0:
                 return
         
+
+    def _do_setup(self, fasta_path, idx_path = None):
+        self.fasta_fh = open(fasta_path)
+        if idx_path is None:
+            idx_path = fasta_path + ".idx"
+        self.idx_fh = open(idx_path)
+        self.idx = mmap.mmap(self.idx_fh.fileno(), 0, access = mmap.ACCESS_READ)
+        self.fasta_path = fasta_path
+        self.idx_path = idx_path
+        self.no_entries = len(self.idx) // 16
+        self.min_mass = self._idkey_at(0)[0]
+        self.max_mass = self._idkey_at(self.no_entries-1)[0]
+        try:
+            self.idx.madvise(mmap.MADV_RANDOM)
+        except Exception:
+            pass
+
+
+    def _idkey_at(self, i):
+        return struct.unpack("dQ", self.idx[i*16:i*16+16])
 
 if __name__ == "__main__":
     import sys
